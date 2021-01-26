cf069fb51fbe50e1cee82ea72b8d88e9a1082b2c
Francis Lavoie <LavFr1932614@etu.cegepjonquiere.ca>
2021-01-26 14:23:05 -0500

    Premier commit

---
+++ Tutoriels/Vaccin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vaccins
+{
+    public class Vaccin
+    {
+        public int VaccinId { get; set; }
+        public DateTime Date { get; set; }
+        public string Nom { get; set; }
+        public string NAMPatient { get; set; }
+
+        public override string ToString()
+        {
+            return $" Vaccin #{VaccinId} ({Nom}), adiminstré le {Date} à {NAMPatient}";
+        }
+    }
+}
---
+++ Tutoriels/VaccinContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vaccins
+{
+    public class VaccinContext : DbContext  
+    {
+        public DbSet<Vaccin> Vaccins { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=VaccinBD;Trusted_Connection=True;");
+    }
+}
