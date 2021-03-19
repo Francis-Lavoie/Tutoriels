1365ee3a4e6c354727a4ec818cae94410618d9f6
Francis Lavoie <LavFr1932614@etu.cegepjonquiere.ca>
2021-03-19 13:30:20 -0400

    Début tuto test unitaires

---
+++ ExoTestUnitaireMock/SuiviVaccinCovid/Program.cs
@@ -0,0 +1,119 @@
+﻿using SuiviVaccinCovid.Modele;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SuiviVaccinCovid
+{
+    public interface IFournisseurDeDate
+    {
+        public DateTime Now { get; }
+    }
+
+    public class FournisseurDeMaintenant : IFournisseurDeDate
+    {
+        public DateTime Now { get { return DateTime.Now; } }
+    }
+
+    public interface IDaoVaccin
+    {
+        public IEnumerable<Vaccin> ObtenirVaccins();
+        public void AjouterVaccin(Vaccin v);
+        public void Sauvegarder();
+    }
+
+    public class DBVaccinContext : IDaoVaccin
+    {
+        private VaccinContext contexte = new VaccinContext();
+        public IEnumerable<Vaccin> ObtenirVaccins()
+        {
+            return contexte.Vaccins;
+        }
+        public void AjouterVaccin(Vaccin v)
+        {
+            contexte.Vaccins.Add(v);
+        }
+        public void Sauvegarder()
+        {
+            contexte.SaveChanges();
+        }
+    }
+
+    public class Program
+    {
+        public IFournisseurDeDate FournisseurDeDate { get; set; }
+        public IDaoVaccin DaoVaccin { get; set; }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program
+            {
+                DaoVaccin = new DBVaccinContext(),
+                FournisseurDeDate = new FournisseurDeMaintenant()
+            };
+            p.Peupler();
+            p.EnregistrerVaccin(p.CreerNouveauVaccin("SIOA95032911", "Pfizer"));
+            Console.WriteLine(p.LePlusRecent());
+        }
+
+        public void Peupler()
+        {
+            if (DaoVaccin.ObtenirVaccins().Count() == 0)
+            {
+                Vaccin dose1Mylene = new Vaccin
+                {
+                    Date = new DateTime(2021, 01, 24),
+                    NAMPatient = "LAPM12345678",
+                    Type = "Moderna"
+                };
+
+                Vaccin dose1Gaston = new Vaccin
+                {
+                    Date = new DateTime(2021, 01, 15),
+                    NAMPatient = "BHEG12345678",
+                    Type = "Pfizer"
+                };
+
+                DaoVaccin.AjouterVaccin(dose1Mylene);
+                DaoVaccin.AjouterVaccin(dose1Gaston);
+
+                DaoVaccin.Sauvegarder();
+            }
+        }
+
+        public Vaccin CreerNouveauVaccin(string nam, string type)
+        {
+            return new Vaccin
+            {
+                NAMPatient = nam,
+                Type = type,
+                Date = DateTime.Now
+            };
+        }
+
+        public void EnregistrerVaccin(Vaccin n)
+        {
+            var memePatient = DaoVaccin.ObtenirVaccins().Where(v => v.NAMPatient == n.NAMPatient);
+            Vaccin vaccin = CreerNouveauVaccin("", "");
+
+            if (memePatient.Count() > 1)
+                throw new ArgumentException("Patient déjà vacciné deux fois");
+            if (memePatient.Count() == 1 && memePatient.First().Type != vaccin.Type)
+                throw new ArgumentException("Un patient ne peut pas recevoir deux" +
+                "types de vaccins");
+
+            DaoVaccin.AjouterVaccin(vaccin);
+            DaoVaccin.Sauvegarder();
+        }
+
+        public Vaccin LePlusRecent(IEnumerable<Vaccin> vaccins)
+        {
+            return vaccins.OrderBy(v => v.Date).Last();
+        }
+
+        public Vaccin LePlusRecent()
+        {
+            return LePlusRecent(DaoVaccin.ObtenirVaccins());
+        }
+    }
+}
---
+++ ExoTestUnitaireMock/SuiviVaccinCovidTests/ProgramTests.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuiviVaccinCovid;
+using SuiviVaccinCovid.Modele;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SuiviVaccinCovid;
+using System.Linq;
+using Moq;
+
+namespace SuiviVaccinCovid.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void LePlusRecentTest()
+        {
+
+            Program p = new Program();
+            List<Vaccin> vaccins = new List<Vaccin> {
+        new Vaccin
+        {
+            NAMPatient = "AAAA10101010",
+            Type = "Pfizer",
+            Date = new DateTime(2021,11,21)
+        },
+        new Vaccin
+        {
+            NAMPatient = "BBBB10101010",
+            Type = "Pfizer",
+            Date = new DateTime(2021,11,30)
+        },
+        new Vaccin {
+            NAMPatient = "CCCC10101010",
+            Type = "Moderna",
+            Date = new DateTime(2021,11,2)
+        }
+        };
+            Vaccin reponse = new Vaccin
+            {
+                NAMPatient = "BBBB10101010",
+                Type = "Pfizer",
+                Date = new DateTime(2021, 11, 30)
+            };
+            Assert.AreEqual(reponse, p.LePlusRecent(vaccins));
+        }
+
+        [TestMethod()]
+        public void LePlusRecentVideTest()
+        {
+            Program p = new Program();
+            List<Vaccin> vaccins = new List<Vaccin>();
+            Assert.ThrowsException<InvalidOperationException>(() => p.LePlusRecent(vaccins));
+        }
+
+        [TestMethod()]
+        public Vaccin LePlusRecent(IEnumerable<Vaccin> vaccins)
+        {
+            if (vaccins.Count() != 0)
+                return vaccins.OrderBy(v => v.Date).Last();
+            return null;
+        }
+
+        [TestMethod()]
+        public void CreerNouveauVaccinTest()
+        {
+            DateTime d = new DateTime(2021, 03, 27, 2, 34, 55, 392);
+            Mock<IFournisseurDeDate> mockFournisseurDate = new Mock<IFournisseurDeDate>();
+            mockFournisseurDate.Setup(m => m.Now).Returns(d);
+            Program p = new Program
+            {
+                FournisseurDeDate = mockFournisseurDate.Object
+            };
+            Vaccin v = new Vaccin
+            {
+                NAMPatient = "AAAA99999999",
+                Type = "ABC",
+                Date = d
+            };
+            Vaccin cree = p.CreerNouveauVaccin("AAAA99999999", "ABC");
+            Assert.AreEqual(v, cree);
+        }
+
+        [TestMethod()]
+        public void EnregistrerVaccinTest()
+        {
+            List<Vaccin> vaccins = new List<Vaccin> {
+            new Vaccin 
+            { 
+                NAMPatient = "AAAA10101010",
+                Type = "Pfizer", 
+                Date = new DateTime(2021,11,21)
+            },
+            new Vaccin 
+            { 
+                NAMPatient = "BBBB10101010", 
+                Type = "Pfizer", 
+                Date = new DateTime(2021,11,30)
+            },
+            new Vaccin 
+            { 
+                NAMPatient = "CCCC10101010",
+                Type = "Moderna", 
+                Date = new DateTime(2021,11,2)},
+            };
+
+            Mock<IDaoVaccin> mockContexte = new Mock<IDaoVaccin>();
+            mockContexte.Setup(m => m.AjouterVaccin(It.IsAny<Vaccin>()));
+            mockContexte.Setup(m => m.ObtenirVaccins()).Returns(vaccins);
+            mockContexte.Setup(m => m.Sauvegarder());
+            
+            Program p = new Program
+            {
+                DaoVaccin = mockContexte.Object
+            };
+            Vaccin v = new Vaccin
+            {
+                NAMPatient = "DDDD10101010",
+                Type = "Pfizer",
+                Date = new DateTime(2021, 03, 27)
+            };
+            
+            p.EnregistrerVaccin(v); 
+            mockContexte.Verify(m => m.AjouterVaccin(v), Times.Once); 
+            mockContexte.Verify(m => m.AjouterVaccin(It.IsAny<Vaccin>()), Times.Once);
+            mockContexte.Verify(m => m.Sauvegarder());
+        }
+    }
+}
